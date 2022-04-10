367b5c70c650a6afe1a4c02293cf44decd03fff4
Digvijay Upadhyay <digvijay.upadhyay@roxorgaming.com>
2022-04-10 12:19:40 +0100

    feat: added script to change font and text color

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,18 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+import { changeBg } from './chrome-utils/index'
+
 function App() {
+  function handleOnTextChange() {
+    changeBg();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+      <header className="App-header" style={{ padding: 40 }}>
+        <div>Actions</div>
+        <button onClick={handleOnTextChange}>change font and color</button>
       </header>
     </div>
   );
---
+++ src/chrome-utils/index.ts
@@ -0,0 +1,26 @@
+export async function changeBg() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id ? tab.id : 1111 },
+    func: setPageBackgroundColor,
+  });
+}
+
+function setPageBackgroundColor() {
+  // 1. Solution one, does not work well since it just changes the 
+  // bg color of the body and not the others
+  // document.body.style.backgroundColor = "#aa44cc";
+
+  var style = document.createElement('style');
+  // creates a problem, it applies to all bt what if the webside has a transparent overlay div (eg. mdn website)
+  const cssStyle = `
+    * {
+      background-color: #FEFBEB !important;
+      color: black !important;
+    }
+  `;
+
+  style.appendChild(document.createTextNode(cssStyle));
+  document.getElementsByTagName('head')[0].appendChild(style);
+}
